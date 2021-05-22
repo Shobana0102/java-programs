21304a6174c6819e94aae5a1ea807c9d89ea145b
Shobana0102 <84520766+Shobana0102@users.noreply.github.com>
2021-05-22 17:55:58 +0530

    Add files via upload

---
+++ MaximumEle.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+class MaximumEle
+{
+    public static void main(String[] args)
+    {
+    int n;
+    int a=0, b=0;
+   
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    int arr[]=new int[n];
+      System.out.println();
+      for(int i=0;i<arr.length;i++)
+       {
+        arr[i]=s.nextInt();
+       }
+    for(int i=0;i<arr.length;i++)
+       {
+        if(a<arr[i])
+         {
+         a=arr[i];
+         }
+       }
+    for(int i=0;i<arr.length;i++)
+     {
+    if(a>arr[i] && b<arr[i])
+     {
+      b=arr[i];
+     }
+    }
+    System.out.println("first maximum element:"+a);
+    }
+}
