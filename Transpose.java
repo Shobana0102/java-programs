faaf7f49aa33d05e94099494f56f70104cbca259
Shobana0102 <84520766+Shobana0102@users.noreply.github.com>
2021-05-22 17:53:00 +0530

    Add files via upload

---
+++ Transpose.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Transpose
+{
+ 
+   public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+           System.out.println();
+            for(int i = 0; i<row; i++)
+           {
+            for(int j = 0; j<col; j++)
+            {
+                arr[i][j] = sc.nextInt();
+            }
+           }
+        System.out.println();
+
+    
+        for(int i = 0; i<row; i++)
+        {
+            for(int j = 0; j<col; j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+           System.out.println();
+        }
+       System.out.println();
+       
+            for(int j = 0; j<col; j++)
+              {
+                 for(int i = 0; i<row; i++)
+                   {
+                System.out.print(arr[i][j]+" ");
+                   }
+                 System.out.println();
+              }
+     
+     }
+  }
