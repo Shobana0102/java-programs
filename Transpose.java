5b4e79cc0d075aec0cbb41333f4fbfb9a1aa7c93
Shobana0102 <84520766+Shobana0102@users.noreply.github.com>
2021-05-23 22:21:40 +0530

    Add files via upload

--- Transpose.java
+++ Transpose.java
@@ -1,3 +1,5 @@
+//Transpose with constraints:(row>3 && row<10),(col>3 && col<10)
+
 import java.util.Scanner;
 class Transpose
 {
@@ -7,36 +9,49 @@ class Transpose
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
         int col = sc.nextInt();
-        int[][] arr = new int[row][col];
+        if(row>3 && row<10)
+        {
+           if(col>3 && col<10)
+           {
+           int[][] arr = new int[row][col];
            System.out.println();
             for(int i = 0; i<row; i++)
-           {
-            for(int j = 0; j<col; j++)
             {
+              for(int j = 0; j<col; j++)
+                 {
                 arr[i][j] = sc.nextInt();
+                 }
             }
-           }
-        System.out.println();
+          System.out.println();
 
     
-        for(int i = 0; i<row; i++)
-        {
-            for(int j = 0; j<col; j++)
+           for(int i = 0; i<row; i++)
             {
+             for(int j = 0; j<col; j++)
+              {
                 System.out.print(arr[i][j]+" ");
+              }
+            System.out.println();
             }
-           System.out.println();
-        }
-       System.out.println();
+             System.out.println();
        
             for(int j = 0; j<col; j++)
               {
                  for(int i = 0; i<row; i++)
-                   {
-                System.out.print(arr[i][j]+" ");
-                   }
-                 System.out.println();
-              }
-     
-     }
-  }
+                  {
+                   System.out.print(arr[i][j]+" ");
+                  }
+                System.out.println();
+               }
+           }
+         else
+           {
+            System.out.println("invalid value");
+           }
+         }
+         else{
+            System.out.println("invalid value");
+         }
+      
+   } 
+}
