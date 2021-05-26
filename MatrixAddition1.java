900da93fe95d09cbba4299c60b00c22bc2b2eebb
Shobana0102 <84520766+Shobana0102@users.noreply.github.com>
2021-05-26 11:09:22 +0530

    Add files via upload

---
+++ MatrixAddition1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class MatrixAddition1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int arr1[][]=new int[row][col];
+        int arr2[][]=new int[row][col];
+        int arr3[][]=new int[row][col];
+        System.out.println();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+             arr1[i][j]=sc.nextInt();
+           }
+        }
+        System.out.println();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+             arr2[i][j]=sc.nextInt();
+           }
+        }
+        System.out.println();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+             arr3[i][j]=(arr1[i][j]+arr2[i][j]);
+            }
+          
+        }
+         for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+            System.out.print(arr3[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
