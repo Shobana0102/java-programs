ab6ff87ee2ef756f54c873166a5cdc858ec7651a
Shobana0102 <84520766+Shobana0102@users.noreply.github.com>
2023-08-19 16:56:42 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,33 +1,61 @@
 import java.util.*;
-public class ArmstrongNumber
+public class Main
 {
-    public static void main(String[] args)//153
-    
+    static int ArmstrongNumber(int a)
     {
-        int n,a,sum=0,temp;
+        int count=0;
+        int arm=0;
+        String b=String.valueOf(a);
+        int c=b.length();
+        while(a>0)
+        {
+            int n=a%10;
+            arm=arm+(int)Math.pow(n,c);
+            a=a/10;
+            count++;
+        }
+        return arm;
         
-        Scanner sc=new Scanner(System.in);
-         n=sc.nextInt();
-         int num=n;
-        while(n>0)
-          {
-           a=n%10;
-           temp=a*a*a;
-           n=n/10;
-           sum=sum+temp;
-
-          }
-   
-   if(sum!=0)
-   {
-     if(sum==num)
-       {
-       System.out.println("Armstrong number");
-       }
-     else
-      {
-       System.out.println("not an armstrong number");
-       }
     }
- }
-}
+    
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int c =ArmstrongNumber(a);
+        System.out.println(c);
+        
+    }
+}// import java.util.*;
+// public class ArmstrongNumber
+// {
+//     public static void main(String[] args)//153
+    
+//     {
+//         int n,a,sum=0,temp;
+        
+//         Scanner sc=new Scanner(System.in);
+//          n=sc.nextInt();
+//          int num=n;
+//         while(n>0)
+//           {
+//            a=n%10;
+//            temp=a*a*a;
+//            n=n/10;
+//            sum=sum+temp;
+
+//           }
+   
+//    if(sum!=0)
+//    {
+//      if(sum==num)
+//        {
+//        System.out.println("Armstrong number");
+//        }
+//      else
+//       {
+//        System.out.println("not an armstrong number");
+//        }
+//     }
+//  }
+// }
